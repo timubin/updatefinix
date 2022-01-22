e8d4ad3d3f0db118a1b5ec1d5fbb2d62d7c0b8a9
timubin <mubin3242@gmail.com>
2022-01-22 23:07:36 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let mobileIcon = document.querySelector(".mobileIcon");
+let navber = document.querySelector(".navbar .primary-manu");
+mobileIcon.addEventListener("click", function () {
+  navber.style = "display:show";
+});
